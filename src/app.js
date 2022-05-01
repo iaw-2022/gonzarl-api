130e196d15a5b6ac334ef30b37f5c6c96c43ca84
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-01 16:37:39 -0300

    api index

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const teamsRoutes = require('./routes/teams.routes');
+const driversInTeamsRoutes = require('./routes/drivers_in_teams.routes');
+
+//app
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+    res.json({
+        message: 'api',
+    })
+});
+
+app.use('/teams', teamsRoutes);
+app.use('/drivers_in_teams', driversInTeamsRoutes);
+
+module.exports = app;
