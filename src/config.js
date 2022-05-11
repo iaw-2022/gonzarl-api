18d3b3c356893230e5531473411e7593230a46e7
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-11 16:04:31 -0300

    changed config for external db

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT,
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_DATABASE: process.env.DB_DATABASE,
-    DB_PASSWORD: process.env.DB_PASSWORD,
+    DATABASE_URL:process.env.DATABASE_URL,
     SERVER: process.env.SERVER,
 }
--- src/database.js
+++ src/database.js
@@ -2,10 +2,7 @@ const { Pool } = require('pg');
 const config = require('./config');
 
 const dbConfig = {
-    user: config.DB_USER,
-    host: config.DB_HOST,
-    password: config.DB_PASSWORD,
-    database: config.DB_DATABASE
+    database: config.DATABASE_URL
 }
 
 const db = new Pool(dbConfig);
