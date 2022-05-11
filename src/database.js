d6acc467070219322acf56870ba20a873a1286ce
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-11 20:48:09 -0300

    fix ssl

--- src/database.js
+++ src/database.js
@@ -2,7 +2,8 @@ const { Pool } = require('pg');
 const config = require('./config');
 
 const dbConfig = {
-    database: config.DATABASE_URL
+    database: config.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 }
 
 const db = new Pool(dbConfig);
