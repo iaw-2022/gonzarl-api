98a0e4b106c231c640b348bf07a4e4a7dcbb0b10
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-12 15:26:41 -0300

    fix crash in deploy v2

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
     DATABASE: process.env.DATABASE,
     DB_HOST: process.env.DB_HOST,
     DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_PORT: process.env.DB_PORT,
+    DB_PORT: process.env.PORT,
     DB_USER: process.env.DB_USER
 }
