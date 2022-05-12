38742b6c4df42f70406755303be10efccd9d9e6d
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-12 15:14:23 -0300

    fix crash in deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT,
-    DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_ENVIRONMENT: process.env.DATABASE_ENVIRONMENT,    
+    DATABASE: process.env.DATABASE,
+    DB_HOST: process.env.DB_HOST,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT,
+    DB_USER: process.env.DB_USER
 }
--- src/database.js
+++ src/database.js
@@ -2,7 +2,11 @@ const { Pool } = require('pg');
 const config = require('./config');
 
 const dbConfig = {
-    database: config.DATABASE_URL,
+    database: config.DATABASE,
+    host: config.DB_HOST,
+    password: config.DB_PASSWORD,
+    port: config.DB_PORT,
+    user: config.DB_USER,
     ssl: { rejectUnauthorized: false }
 }
 
