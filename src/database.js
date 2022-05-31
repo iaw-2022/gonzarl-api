48cc81c360325cc83397a0566f3d58a30425e8c6
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-31 20:10:48 -0300

    Agrego ssl a database

--- src/database.js
+++ src/database.js
@@ -5,7 +5,8 @@ const dbConfig = {
     user: config.DB_USER,
     host: config.DB_HOST,
     password: config.DB_PASSWORD,
-    database: config.DB_DATABASE
+    database: config.DB_DATABASE,
+    ssl: { rejectUnauthorized: false }
 }
 
 const db = new Pool(dbConfig);
