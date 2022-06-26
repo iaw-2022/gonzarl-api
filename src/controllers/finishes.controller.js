d9c6df81ec70601fb005b0ca7474a3c469909d91
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-06-26 16:07:04 -0300

    Correccion getFinishesByRace

--- src/controllers/finishes.controller.js
+++ src/controllers/finishes.controller.js
@@ -18,18 +18,28 @@ const getFinishesByRaceId = async (req, res) => {
         if (checkExistsFinishes.rowCount > 0) {
             const response = await db.query('SELECT id, race_id, driver_1_id, driver_2_id, driver_3_id, driver_4_id, driver_5_id, driver_6_id, driver_7_id, driver_8_id, driver_9_id, driver_10_id FROM FINISHES WHERE race_id = $1', [req.params.id]);
             if (response.rows.length > 0 ){
-                const drivers = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1 OR id = $2 OR id = $3 OR id = $4 OR id = $5 OR id = $6 OR id = $7 OR id = $8 OR id = $9 OR id = $10', 
-                    [response.rows[0].driver_1_id,
-                    response.rows[0].driver_2_id,
-                    response.rows[0].driver_3_id,
-                    response.rows[0].driver_4_id,
-                    response.rows[0].driver_5_id,
-                    response.rows[0].driver_6_id,
-                    response.rows[0].driver_7_id,
-                    response.rows[0].driver_8_id,
-                    response.rows[0].driver_9_id,
-                    response.rows[0].driver_10_id,]);
-                res.status(200).json(drivers.rows);
+                let drivers = [];
+                let driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_1_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_2_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_3_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_4_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_5_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_6_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_7_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_8_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_9_id]);
+                drivers.push(driver.rows[0])
+                driver = await db.query('SELECT id, name, scuderia FROM drivers WHERE id = $1', [response.rows[0].driver_10_id]);
+                drivers.push(driver.rows[0])
+                res.status(200).json(drivers);
             }else {
                 res.status(404).json({error: 'not found.'});
             }
